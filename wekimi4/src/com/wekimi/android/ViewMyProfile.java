d246929a7c7ce22b1a4e71a41305afeff7002cce
Sooyeon <cheval1201@gmail.com>
2014-02-20 00:26:06 +0900

    message sending problem solved. before combining with sungeun's files

---
+++ wekimi4/src/com/wekimi/android/ViewMyProfile.java
@@ -0,0 +1,17 @@
+package com.wekimi.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewMyProfile extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewmyprofile);
+
+	    // TODO Auto-generated method stub
+	}
+
+}
